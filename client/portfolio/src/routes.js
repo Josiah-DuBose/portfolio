4eb15c46ab62ede0bc8667f7a9f0d984018a160e
Josiah DuBose <josiah7686@gmail.com>
2021-08-14 20:40:01 -0400

    switch to angular

--- client/portfolio/src/routes.js
+++
@@ -1,9 +0,0 @@
-import { Route, Switch } from 'react-router-dom';
-
-import Home from './pages/Home';
-
-export default (
-    <Switch>
-        <Route exact path="/" component={Home}></Route>
-    </Switch>
-);
